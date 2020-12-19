695b5d4530d30bc16aff3cbec0a7e56bf1921f4d
amooryjubran <omarsalehjubran@gmail.com>
2020-12-19 16:38:05 -0500

    Done

---
+++ src/components/Search-Box.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SearchBox({ placeHolder, handleChange }) {
+  return (
+    <div className="search-center">
+      <input
+        className="search"
+        type="search"
+        placeholder={placeHolder}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
